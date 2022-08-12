b935b9eb596ea8d42cf4466d969e5e97ac713e99
LAPTOP-SRCMU64U\Renato <you@example.com>
2022-08-12 20:45:22 +0100

    Seed context created

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.API.Models;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+
+      
+        IMongoCollection<Product> Products { get; }
+    }
+}
--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -1,10 +1,17 @@
-﻿namespace Catalog.API.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Models
 {
     public class Product
     {
-
-       public string Id  { get; set; } 
-
-
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
     }
 }
