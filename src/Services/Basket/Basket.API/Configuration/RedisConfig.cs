0a0d1a6c754aa7abfad4db708f86e46b82bf4c63
LAPTOP-SRCMU64U\Renato <you@example.com>
2022-08-15 19:52:25 +0100

    Redis configuration

---
+++ src/Services/Basket/Basket.API/Configuration/RedisConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Basket.API.Configuration
+{
+    public static class RedisConfig
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddStackExchangeRedisCache(opt => opt.Configuration="localhost:6379");
+           
+        }
+    }
+}
